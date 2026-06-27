8d739a8e0776e80fd4d5f74139b8090eafb722f3
Htet Khaing Win <p.htetkhaingwin@gmail.com>
2026-06-27 14:35:20 +0800

    Added: Seperate Embedding Model

---
+++ rag/embedder.py
@@ -0,0 +1,10 @@
+import ollama
+
+def embed_text(text: str) -> list[float]:
+    """Get embedding vector for a text using Ollama API"""
+    response = ollama.embeddings(model='nomic-embed-text', prompt=text)
+    return response['embedding'] # list of 768 floats
+
+def embed_batch(texts: list[str]) -> list[list[float]]:
+    """Get embedding vectors for a batch of texts using Ollama API"""
+    return [embed_text(t) for t in texts] 
